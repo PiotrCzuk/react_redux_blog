d5428061b229c6cf2cdc357de4c0395360ae7906
PiotrCzuk <p.michalc@gmail.com>
2019-03-21 14:35:26 +0100

    Refactor dumping the memoize function in favour of nested actionCreators

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,14 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+
 export const fetchPosts = () =>
     async dispatch => {
         const response = await jsonPlaceholder.get('/posts');
@@ -9,11 +17,20 @@ export const fetchPosts = () =>
             type: 'FETCH_POSTS', payload: response.data
         });
     };
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({
         type: 'FETCH_USER', payload: response.data
     });
-});
+};
+
+
+// Memoized version of reducing number of requests.
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//         type: 'FETCH_USER', payload: response.data
+//     });
+// });
